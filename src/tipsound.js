4255748ce74451629039dd3bd693f4634a5e87aa
ruly-rudel <ruly.official@gmail.com>
2015-04-17 09:45:37 +0900

    refactoring

--- src/tipsound.js
+++ src/tipsound.js
@@ -24,46 +24,51 @@ function inherit(p) {
     return new f();
 }
 
+
+Function.prototype.method = function (name, func) {
+    if (!this.prototype[name]) {
+        this.prototype[name] = func;
+        return this;
+    } else {
+        throw new Error("method error.");
+    }
+};
+
+Number.method('integer', function () {
+    return Math[this < 0 ? 'ceil' : 'floor'](this);
+});
+
 /*
-function monotone(freq, sec)
-{
-    var tone = function (t, srate) { return Math.sin(freq / srate * Math.PI * t); }
-    tone.dulation = sec;    // in second
-
-    return tone;
-}
-
-function mix()
-{
-    var oargs = arguments;
-
-    if (oargs.length > 1) {
-        var v = reduce.call(
-                    oargs,
-                    function (x, y) {
-                        return function () {
-                            return x.apply(this, arguments) + y.apply(this, arguments);
-                        };
-                    });
-        v = compose(function (x) { return x / oargs.length; } , v);
-        v.dulation = oargs[0].dulation; // to be fixed(MAX of oargs[*].dulation)
-        return v;
-    }
-    else
-    {
-        return oargs[0];
-    }
-}
+String.method('trim', function () {
+    return this.replace(/^\s+|\s+$/g, '');
+});
 */
+
+Function.method('curry', function () {
+    var slice = Array.prototype.slice,
+    args = slice.apply(arguments),
+    that = this;
+    return function () {
+        return that.apply(null, args.concat(slice.apply(arguments)));
+    };
+});
+
+var memoizer = function (memo, fundamental) {
+    var shell = function (n) {
+        var result = memo[n];
+        if (typeof result !== 'number') {
+            result = fundamental(shell, n);
+            memo[n] = result;
+        }
+        return result;
+    };
+    return shell;
+};
+
 
 function constant(v)
 {
     return function (t) { return v; }
-}
-
-function osc_sin(freq, srate)
-{
-   return function (t) { return Math.sin(freq(t, srate) / srate * 2 * Math.PI * t); }
 }
 
 function gain(gf, of)
@@ -96,20 +101,24 @@ function note(n, octave) {
     return Math.pow(2, (n - 33 + (12 * (octave || 0))) / 12) * 440;
 }
 
-function TipSound(srate)
+var tipsound = function(srate)
 {
+    // private values
+    var that = {};
+
+    srate = srate || 44100;
+
     try {
-        this.context = new AudioContext();
+        that.context = new AudioContext();
     }
     catch(e)
     {
         console.log(e);
     }
+    that.context.samplingRate = srate;
 
-    this.context.samplingRate = srate ? srate : 44100;
-
-    this.play = function (seq, len) {
-        var buffer = this.context.createBuffer(1, this.context.samplingRate * len, this.context.samplingRate);
+    that.play = function (seq, len) {
+        var buffer = this.context.createBuffer(1, srate * len, srate);
         var channel = buffer.getChannelData(0);
 
         for (var i = 0; i < channel.length; i++) {
@@ -124,21 +133,23 @@ function TipSound(srate)
         src.start(this.context.currentTime);
     }
 
-    this.osc_sin = function (freq) {
-        return function (t) { return Math.sin(freq(t) / this.context.samplingRate * 2 * Math.PI * t); }
+    that.osc_sin = function (freq) {
+        return function (t) { return Math.sin(freq(t) / srate * 2 * Math.PI * t); }
     }
 
-    this.osc_saw = function (freq)
+    that.osc_saw = function (freq)
     {
-        return function (t) { var p = t % (this.context.samplingRate / freq(t)); return 1 - 2 * p / freq(t); }
+        return function (t) { var p = t % (srate / freq(t)); return 1 - 2 * p / freq(t); }
     }
 
-    this.sequence_freq = function (seq, bmp)
+    that.sequence_freq = function (seq, bmp)
     {
-        var nt = this.context.samplingRate * 60 / (bmp * 4); // 1/4
+        var nt = srate * 60 / (bmp * 4); // 1/4
 
-        return function (t) { var p = t / nt; return note(seq[(p % seq.length).toFixed(0)]); }
+        return function (t) { var p = t / nt; return note(seq[(p % seq.length).integer()]); }
     }
+
+    return that;
 }
 
 
@@ -147,7 +158,7 @@ function TipSound(srate)
 
 
 
-var ts = new TipSound();
+var ts = tipsound();
 //document.querySelector("#play").addEventListener("click", function () { ts.play(mix(osc_sin(constant(440), constant(44100)), osc_sin(constant(440 * 1.26), constant(44100)), osc_sin(constant(440 * 1.498), constant(44100))), 1.0); }, false);
 //document.querySelector("#play").addEventListener("click", function () { ts.play(mix(ts.osc_saw(constant(440)), ts.osc_saw(constant(440*1.26)), ts.osc_saw(constant(440*1.498))), 1.0); }, false);
 //document.querySelector("#play").addEventListener("click", function () { ts.play(ts.osc_saw(constant(440 / 2)), 1.0); }, false);
